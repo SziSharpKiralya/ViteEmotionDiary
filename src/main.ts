374c617c6b57910057c2f5ea4b6312950dd3ed14
Csengeri Marcell <csengeri.marcell@petrik.hu>
2026-09-07 10:13:10 +0200

    Form done.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-import "./style.css"
-import "./error26.css"
+import "./style.css";
+//import "./error26.css"
+import type { Emotions } from "./Emotions";
+
+const API_URL = "https://retoolapi.dev/xJiRs2/data";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("diary_form")?.addEventListener("submit", addEmotion);
+});
+
+async function addEmotion(e: SubmitEvent) {
+  e.preventDefault();
+}
